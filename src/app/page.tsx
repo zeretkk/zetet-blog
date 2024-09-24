20378e28db7e949c793c369bc67d0098f30a4080
zeretkk <kirill03192@gmail.com>
2024-09-24 11:50:03 +0300

    remove unneeded debug thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use server'
 import { Hero } from '@/components/Hero'
-import { notFound } from 'next/navigation'
 
 export default async function Home() {
-  notFound()
   return (
     <main>
       <Hero />
