fe486165de2e4e6880026c9919481f6b0352502f
zeretkk <kirill03192@gmail.com>
2024-10-03 17:55:59 +0300

    remove yandex verify meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { Hero } from '@/components/Hero'
 export default async function Home() {
   return (
     <main>
-      <head>
-        <meta name='yandex-verification' content='e5af7afc231bbb15' />
-      </head>
       <Hero />
     </main>
   )
