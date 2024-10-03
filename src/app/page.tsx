84dc00fdb9523fc957b334b1e330a19ef46ba95d
zeretkk <kirill03192@gmail.com>
2024-10-03 10:03:56 +0300

    add yandex verification meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { Hero } from '@/components/Hero'
 export default async function Home() {
   return (
     <main>
+      <head>
+        <meta name='yandex-verification' content='e5af7afc231bbb15' />
+      </head>
       <Hero />
     </main>
   )
